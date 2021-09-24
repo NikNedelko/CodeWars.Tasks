3043e8eb0584ce2c84f5ab8717f6efb8b89adb44
Nikita <nedelko1nik@gmail.com>
2021-09-24 12:30:15 +0300

    first commit

---
+++ 6Kyu/Hi numb/HiNumbers/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HiNumbers
+{
+    class Kata{
+
+     static List<int> repeatedNumbers = new List<int>();
+     public static int HighestRank (int[] arr){
+         int counter = new int();
+         int isRepeated = new int();
+         bool ifValueWasRepeated= new bool();
+         int result = new int();
+         int counterOfNUMBER = new int();
+         Array.Sort(arr);
+         for (int i = 0; i < arr.Length; i++)
+         {
+             ifValueWasRepeated = valueWasRepeated(arr[i]);
+             if (ifValueWasRepeated == true)
+             {
+                continue; 
+             }
+          for (int y = 0; y < arr.Length; y++)
+          {
+              if(arr[i]==arr[y]){
+                  if (isRepeated == 0)
+                  {
+                      isRepeated++;
+                  }
+                  counter++;
+                  continue;  
+              }
+              else
+              {
+                  continue;
+              }
+          }
+          //Console.WriteLine(array[i]+ " was repeated = " + counter);
+          if (counter>counterOfNUMBER)
+          {
+              result = arr[i];
+              counterOfNUMBER = 0;
+              counterOfNUMBER += counter;
+              counter=0;
+              isRepeated=0;
+              continue;
+          }
+          
+             
+              if (counterOfNUMBER == counter)
+              {
+                  if (result>arr[i])
+                  {
+                     counterOfNUMBER = 0;
+                     counterOfNUMBER += counter;
+                     counter=0;
+                     isRepeated=0; 
+                     continue;
+                  }
+              
+              else{
+                  result = arr[i];
+                  counterOfNUMBER = 0;
+                  counterOfNUMBER += counter;
+                  counter=0;
+                  isRepeated=0;
+                  continue;
+              }
+              }
+              counter=0;
+              isRepeated=0; 
+              
+               
+         }
+       
+         repeatedNumbers.Clear();   
+         return result;
+    }
+    static bool valueWasRepeated(int value){
+        foreach (int item in repeatedNumbers)
+        {
+            if (value!=item)
+            {
+            }
+            else
+            {
+                return true;
+            }
+        }
+        repeatedNumbers.Add(value);
+        return false;
+    }
+}
+
+//////////////////
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstArr = new int[] { 12,10,8,12,7,6,4,10,12 };
+            var secArr = new int[] { 12,10,8,12,7,6,4,10,10 };
+            int firstResult = Kata.HighestRank(firstArr);
+           
+            System.Console.WriteLine(firstResult);
+
+            if(firstResult==10){
+                Console.WriteLine("Mission complete");
+            }
+            else{
+                System.Console.WriteLine("переделывай");
+            }
+
+            int secondResult = Kata.HighestRank(secArr);
+
+              System.Console.WriteLine(secondResult);
+            if(secondResult==10){
+                Console.WriteLine("Mission complete");
+            }
+            else{
+                System.Console.WriteLine("переделывай");
+            } 
+        }
+    }
+}
---
+++ 6Kyu/Tribonachi/Tribonacci_Seq/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tribonacci_Seq
+{
+    public class Xbonacci
+{
+  public double[] Tribonacci(double[] signature, int n)
+  {
+       double[] exportArr = new double[n];
+           if (exportArr.Length<3)
+      {
+          for (int y = 0; y < n; y++)
+          {
+              exportArr[y]+=signature[y];
+          }
+            return exportArr;
+      }
+      exportArr[0]= signature[0];
+      exportArr[1]= signature[1];
+      exportArr[2]= signature[2];
+      for (int i = 3; i < exportArr.Length; i++)
+      {
+          exportArr[i] += exportArr[i-1] + exportArr[i-2] + exportArr[i-3];
+          for (int ex = 0; ex < exportArr.Length; ex++)
+          {
+              System.Console.Write(exportArr[ex] + ",");
+          }
+          System.Console.WriteLine(" ");
+      }
+    System.Console.WriteLine("passed");
+    return exportArr;
+  }
+}
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           double[] arr = new double[] {19,1,3}; //put your numbers here
+           int howMany =2; // how large array u need
+           Xbonacci xbonacci = new Xbonacci();
+           double[] result = xbonacci.Tribonacci(arr,howMany);
+
+             for (int eex = 0; eex < result.Length; eex++)
+          {
+              System.Console.WriteLine(result[eex]);
+          }
+            System.Console.ReadLine();
+        }
+    }
+}
